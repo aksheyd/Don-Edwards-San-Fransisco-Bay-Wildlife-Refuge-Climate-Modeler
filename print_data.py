dd94aa7bb4de14bb8417f1f74217942c99c26a6b
aksheyd <aksheyd@umich.edu>
2022-02-06 23:27:55 -0500

    edited print

--- print_data.py
+++ print_data.py
@@ -10,10 +10,11 @@ bs_df = bs_df[:270]
 print("Birds: ")
 print("======")
 print()
-print(bs_df)
+print(bs_df[["Common Name", "Scientific Name", "Danger Level"]].head())
 print("Max Danger Level for Birds = ", bs_df["Danger Level"].max())
 print("Min Danger Level for Birds = ", bs_df["Danger Level"].min())
 print("Average Danger Level for Birds = ", bs_df["Danger Level"].mean())
+print("Amount of Birds = ", len(bs_df))
 os.remove("output_birds.csv")
 
 m_df = pd.read_csv(r'/Users/aksheydeokule/Documents/EcoData S2/Climate Modeler/output_mammals.csv')
@@ -21,10 +22,11 @@ m_df = m_df[:29]
 print("Mammals: ")
 print("======== ")
 print()
-print(m_df.head())
+print(m_df[["Common Name", "Scientific Name", "Danger Level"]].head())
 print("Max Danger Level for Mammals = ", m_df["Danger Level"].max())
 print("Min Danger Level for Mammals = ", m_df["Danger Level"].min())
 print("Average Danger Level for Mammals = ", m_df["Danger Level"].mean())
+print("Amount of Mammals = ", len(m_df))
 os.remove("output_mammals.csv")
 # ADD COLUMNS AND ROW INFO
 
