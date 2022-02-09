befa3308018eb2c3bcd7b22da4c49ad77c2e0735
aksheyd <aksheyd@umich.edu>
2022-02-08 19:52:38 -0500

    added everything but flora to sheets

--- sheet_analyzer.py
+++ sheet_analyzer.py
@@ -21,6 +21,14 @@ bs_df = bs_df[:270]
 m_df = pd.read_csv (r'/Users/aksheydeokule/Documents/EcoData S2/Climate Modeler/MammalsSheet.csv')
 m_df = m_df[:30] 
 
+# Amphibians/Reptiles CSV
+ar_df = pd.read_csv (r'/Users/aksheydeokule/Documents/EcoData S2/Climate Modeler/AmphibianReptilesSheet.csv')
+ar_df = ar_df[:14] 
+
+# Reptiles CSV
+f_df = pd.read_csv (r'/Users/aksheydeokule/Documents/EcoData S2/Climate Modeler/FishsSheet.csv')
+f_df = f_df[:58] 
+
 #converts Occurrence column into string for if statements in DangerLevel func
 bs_df["Occurrence"] = bs_df["Occurrence"].astype(str)
 bs_df["Classification"] = bs_df["Classification"].astype(str)
@@ -31,6 +39,16 @@ m_df["Occurrence"] = m_df["Occurrence"].astype(str)
 m_df["Classification"] = m_df["Classification"].astype(str)
 m_df["Federal"] = m_df["Federal"].astype(str)
 m_df["State"] = m_df["State"].astype(str)
+
+ar_df["Occurrence"] = ar_df["Occurrence"].astype(str)
+ar_df["Classification"] = ar_df["Classification"].astype(str)
+ar_df["Federal"] = ar_df["Federal"].astype(str)
+ar_df["State"] = ar_df["State"].astype(str)
+
+f_df["Occurrence"] = f_df["Occurrence"].astype(str)
+f_df["Classification"] = f_df["Classification"].astype(str)
+f_df["Federal"] = f_df["Federal"].astype(str)
+f_df["State"] = f_df["State"].astype(str)
 
 def DangerLevel (row1):
     returnVal = 0
@@ -45,12 +63,25 @@ def DangerLevel (row1):
     elif (row1["Occurrence"] == "accidental"):
         returnVal += 10
 
+
     # Solo habitat lowers survival chance (LOOK INTO CHANGING CLIMATE BASED ON HABTIAT)
     if ((row1["Occurrence"] == "upland") or (row1["Occurrence"] == "marsh") or (row1["Occurrence"] == "tidal sloughs")):
         returnVal += 10
     # Multiple habitats means greater survival in more areas
     elif ((row1["Occurrence"] == "all habitats")):
         returnVal += 5
+
+
+    # Solo habitat lower survival chance (LOOK INTO CHANGING CLIMATE BASED ON HABTIAT))
+    if (row1["Occurrence"] == "present in tidal waters"):
+        returnVal += 10
+    # More habitats -> greater survival
+    elif (row1["Occurrence"] == "present in tidal waters and ponds"):
+        returnVal += 5
+    # Rare occurence means lower population and lower survival
+    elif (row1["Occurrence"] == "rare in tidal waters"):
+        returnVal += 20
+
 
     # Native species, like locally nested, have better climate acclimation
     if (row1["Classification"] == "native"):
@@ -72,8 +103,8 @@ def DangerLevel (row1):
     # Threatened species are close to becoming endangered
     elif (row1["State"] == "T"):
         returnVal += 25
-    # SSC (Species of Special Concern) is a CA specific classification for threatened and declining population species
-    elif (row1["State"] == "SSC"):
+    # SSC/SC (Species of Special Concern) is a CA specific classification for threatened and declining population species
+    elif (row1["State"] == "SSC") or (row1["State"] == "SC"):
         returnVal += 50
     # SP means State Protected 
     elif ((row1["State"] == "SP") or (row1["State"] == "FP")):
@@ -84,11 +115,17 @@ def DangerLevel (row1):
 # Creates Danger Level column
 bs_df.insert(len(bs_df.columns), 'Danger Level', 0)
 m_df.insert(len(m_df.columns), 'Danger Level', 0)
+ar_df.insert(len(ar_df.columns), 'Danger Level', 0)
+f_df.insert(len(f_df.columns), 'Danger Level', 0)
 
 # Iterates through rows to calculate Danger Level
 bs_df['Danger Level'] = bs_df.apply(lambda row1: DangerLevel(row1), axis = 1)
 m_df['Danger Level'] = m_df.apply(lambda row1: DangerLevel(row1), axis = 1)
+ar_df['Danger Level'] = ar_df.apply(lambda row1: DangerLevel(row1), axis = 1)
+f_df['Danger Level'] = f_df.apply(lambda row1: DangerLevel(row1), axis = 1)
 
+# Turns all datasets into csv files
 bs_df.to_csv("output_birds.csv", index = False)
 m_df.to_csv("output_mammals.csv", index = False)
-#print(bs_df)
+ar_df.to_csv("output_amphibianreptiles.csv", index = False)
+f_df.to_csv("output_fishs.csv", index = False)
