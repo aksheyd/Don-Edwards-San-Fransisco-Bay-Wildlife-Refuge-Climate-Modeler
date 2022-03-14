24d48a77a1c1c319ac2d196bbf1dd80f3736057f
aksheyd <aksheyd@umich.edu>
2022-03-14 18:08:01 -0400

    upgraded cla's

--- main.cpp
+++ main.cpp
@@ -13,8 +13,6 @@ void modeler_main();
 void instructions();
 void change_climate(int input);
 
-
-
 int main() {
     int input;
 
@@ -54,24 +52,24 @@ void DESFB_overview() {
     cout << "Species: 269 Birds, 28 Mammals, 12 Amphibian/Reptiles, 62 Fish, 335 Fauna" << endl;
     cout << endl;
 
-    /*
+    
     std::this_thread::sleep_for(chrono::nanoseconds(10));
-    std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::seconds(15));
+    std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::seconds(1));
     system("clear && printf \'\\e[3J\'");
-    */
+    
 
     cout << "Here's a sneakpeek at some of the birds, mammals, etc" << endl;
     cout << "For the Danger Level coloumn, this number is calculated based on the inital data given" << endl <<
             "from the US Fish & Wildlife Service. It is based on many factors including the species" << endl <<
             "origin, endangered-level, etc." << endl;
-    /*
+    
     std::this_thread::sleep_for(chrono::nanoseconds(10));
-    std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::seconds(4));
-    */
+    std::this_thread::sleep_until(std::chrono::system_clock::now() + std::chrono::seconds(1));
+    
 
     string filename = "\"/Users/aksheydeokule/Documents/EcoData S2/Climate Modeler/sheet_analyzer.py\"";
     string command = "python3 ";
-    command += filename;
+    command += filename + " -p 0"; //accounts for command line arguements
     system("clear && printf \'\\e[3J\'");
     system(command.c_str());
 
@@ -95,19 +93,37 @@ void modeler_main() {
     cout << "Now, it\'s your turn! Use the following instructions to affect the climate:" << endl << endl;
     instructions();
     cin >> input;
+
+    while (input < 0 || input > 99) {
+        cout << "Error: incorrect input detected." << endl;
+        instructions();
+        cin >> input;
+    }
     change_climate(input);
     return;
 }
 
 void instructions() {
     // Make this a more robust command system, for now its just the desired temperature
-    cout << "Input a temperature from 0 to 100 degrees Fahrenheit." << endl;
-    cout << "Ex: 50, 95, 100, 5, etc." << endl;
+    cout << "Input a temperature from 0 to 99 degrees Fahrenheit." << endl;
+    cout << "Ex: 50, 95, 99, 5, etc." << endl;
 }
 
 void change_climate(int input) {
     if (input > 50) {
-        cout << " dead rip" << endl;
+        string inp_s = to_string(input);
+        string filename = "\"/Users/aksheydeokule/Documents/EcoData S2/Climate Modeler/sheet_analyzer.py\"";
+        string command = "python3 ";
+        command += filename + " -p " + inp_s; //accounts for command line arguements
+        cout << command << endl;
+        system("clear && printf \'\\e[3J\'");
+        system(command.c_str());
+ 
+        filename = "\"/Users/aksheydeokule/Documents/EcoData S2/Climate Modeler/print_data.py\"";
+        command = "python3 ";
+        command += filename;
+        system("clear && printf \'\\e[3J\'");
+        system(command.c_str());
     }
     int hel;
     cin >> hel;
